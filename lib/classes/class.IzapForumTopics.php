a54fad63c6c7112d7a50254dd05329f2171208b8
Carl <carl@izap.in>
2001-09-15 01:54:36 +0000

    Full Update

--- lib/classes/class.IzapForumTopics.php
+++ lib/classes/class.IzapForumTopics.php
@@ -21,18 +21,19 @@ class IzapForumTopics extends IzapObject {
 
   public function get_attributes_array() {
     return array(
-      'title' => array('required' => TRUE, 'type' => 'string'),
-      'description' => array('required' => TRUE, 'type' => 'text'),
-      'access_id' => array('required' => TRUE, 'type' => 'numerical', 'default' => ACCESS_DEFAULT),
-      'tags' => array('required' => FALSE, 'type' => 'string'),
-      'parent_guid' => array('required' => TRUE, 'type' => 'numerical', 'default' => 0),
-      'category_guid' => array('required' => TRUE, 'type' => 'numerical', 'default' => 0),
-      'forum_main_topics' => array('required' => TRUE, 'type' => 'string', 'default' => 'no'),
+            'title' => array('required' => TRUE, 'type' => 'string'),
+            'description' => array('required' => TRUE, 'type' => 'text'),
+            'access_id' => array('required' => TRUE, 'type' => 'numerical', 'default' => get_default_access()),
+            'tags' => array('required' => FALSE, 'type' => 'tags'),
+            'parent_guid' => array('required' => TRUE, 'type' => 'numerical', 'default' => 0),
+            'category_guid' => array('required' => TRUE, 'type' => 'numerical', 'default' => 0),
+            'forum_main_topics' => array('required' => TRUE, 'type' => 'string', 'default' => 'no'),
+            'sticky_topic' => array('required' => TRUE, 'type' => 'string', 'default' => 'no')
     );
   }
 
   public function getForumParams() {
-   $forum_params = array_keys($this->get_attributes_array());
+    $forum_params = array_keys($this->get_attributes_array());
 
     $return = new stdClass();
     $return->guid = $this->guid;
@@ -44,7 +45,7 @@ class IzapForumTopics extends IzapObject {
   }
 
   public function getFormParams() {
-   $forum_params = array_keys($this->get_attributes_array());
+    $forum_params = array_keys($this->get_attributes_array());
 
     $return = new stdClass();
     $return->guid = $this->guid;
@@ -57,10 +58,10 @@ class IzapForumTopics extends IzapObject {
 
   public function getUrl() {
     global $CONFIG;
-    $initial_url = $CONFIG->wwwroot . 'pg/forum/';
+    $initial_url = $CONFIG->wwwroot . 'pg/forums/';
     if($this->forum_main_topics == 'yes') {
       return $initial_url . 'list/' . $this->category_guid . '/' . $this->guid . '/' . friendly_title($this->title);
-    }else{
+    }else {
       return $initial_url . 'topic/' . $this->category_guid . '/' . $this->parent_guid . '/' . $this->guid . '/' . friendly_title($this->title);
     }
   }
@@ -71,6 +72,39 @@ class IzapForumTopics extends IzapObject {
     }
 
     return FALSE;
+  }
+
+  public function delete() {
+    // decrease total count, from parent
+    $parent = get_entity($this->parent_guid);
+    func_decrease_total_topics_byizap($parent);
+
+    // decrease total count, from category
+    $category = get_entity($this->category_guid);
+    func_decrease_total_topics_byizap($category);
+
+    // decrease total post count
+    func_izap_update_metadata(array(
+            'entity' => $parent,
+            'metadata' => array(
+                    'total_posts' => (int)$parent->total_posts - $this->countAnnotations(),
+            ),
+    ));
+
+    $options = array(
+            'type' => 'object',
+            'subtype' => 'IzapForumTopics',
+    );
+    $options['metadata_name_value_pairs'][] = array('name' => 'parent_guid', 'value' => $this->guid);
+    $options['limit'] = 9999;
+    $child_entities = elgg_get_entities_from_metadata($options);
+    if($child_entities) {
+      foreach($child_entities as $child) {
+        $child->delete();
+      }
+    }
+    
+    return parent::delete();
   }
 }
 
