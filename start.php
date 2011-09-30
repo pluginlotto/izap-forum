4e4a0f6a3aecab1f47f2c9a5f157acf8c024b6a1
Narinder kaur <narinder.kaur@izap.in>
2011-09-30 15:12:25 +0000

    REMOVED BRIDGE DEPENDENCY BUG

--- start.php
+++ start.php
@@ -18,12 +18,8 @@ define('GLOBAL_IZAP_FORUM_CATEGORY_SUBTYPE', 'IzapForumCategories');
 define('GLOBAL_IZAP_FORUM_TOPIC_SUBTYPE','IzapForumTopics');
 define('GLOBAL_IZAP_FORUM_TOPIC_CLASS','IzapForumTopic');
 // This will escape us from fatal error occurance when izap-bridge got deactivated after forum plugin activation
- if (elgg_is_active_plugin(GLOBAL_IZAP_ELGG_BRIDGE)) {
+ if (elgg_is_active_plugin(GLOBAL_IZAP_ELGG_BRIDGE))
     elgg_register_event_handler('init', 'system', 'izap_forum_init');
-  } else {
-    register_error('This plugin needs izap-elgg-bridge');
-    disable_plugin(GLOBAL_IZAP_FORUM_PLUGIN);
-  }
 
 
 function izap_forum_init() {
